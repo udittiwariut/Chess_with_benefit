c9b52806e5374eb334da03b957b9fe4a56ef7588
udittiwariut <udittiwariut@gmail.com>
2023-10-24 18:03:41 +0530

    404 added

---
+++ src/component/view/404Page/Page404.tsx
@@ -0,0 +1,11 @@
+const Page404 = () => {
+	return (
+		<>
+			<div className="h-screen w-screen bg-lightTile flex justify-center content-center flex-col">
+				<p className="font-sans text-white text-[8.75rem] text-center">404</p>
+			</div>
+		</>
+	);
+};
+
+export default Page404;
